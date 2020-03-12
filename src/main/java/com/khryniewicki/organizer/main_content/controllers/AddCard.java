eec2f7c63813713135da17fb400c0ef277fcf720
Konrad Hryniewicki <konrad.hryniewicki@gmail.com>
2020-03-12 12:03:45 +0100

    second commit

--- src/main/java/com/khryniewicki/organizer/main_content/controllers/AddCard.java
+++ src/main/java/com/khryniewicki/organizer/main_content/controllers/AddCard.java
@@ -1,8 +1,12 @@
 package com.khryniewicki.organizer.main_content.controllers;
 
+import com.khryniewicki.organizer.main_content.model.Task;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AddCard {
@@ -10,5 +14,9 @@ public class AddCard {
     @GetMapping("/addcard")
     public String addCard(Model model) {
         return "main/addCard";
+    }
+    @PostMapping ("/addcard")
+    public String postCard(@ModelAttribute Task task){
+        return "redirect:/dashboard";
     }
 }
