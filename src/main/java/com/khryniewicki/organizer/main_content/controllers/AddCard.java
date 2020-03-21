104375dc32a6386a00aea165abb9e0d6323a1d86
Konrad <konrad.hryniewicki@gmail.com>
2020-03-21 21:54:53 +0100

    -on linux

--- src/main/java/com/khryniewicki/organizer/main_content/controllers/AddCard.java
+++ src/main/java/com/khryniewicki/organizer/main_content/controllers/AddCard.java
@@ -1,22 +1,7 @@
 package com.khryniewicki.organizer.main_content.controllers;
 
-import com.khryniewicki.organizer.main_content.model.Task;
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class AddCard {
+public class AddCard {}
 
-    @GetMapping("/addcard")
-    public String addCard(Model model) {
-        return "main/addCard";
-    }
-    @PostMapping ("/addcard")
-    public String postCard(@ModelAttribute Task task){
-        return "redirect:/dashboard";
-    }
-}
